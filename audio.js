0d5c5fcac7a49b03c958bc2ede571c6f6afa9932
lukamusashvili <57059505+lukamusashvili@users.noreply.github.com>
2021-06-20 00:39:40 +0400

    faster

--- audio.js
+++ audio.js
@@ -1,51 +1,63 @@
 document.addEventListener('keydown', function(event) {
     if(event.key == "a"){ //C
-        var audio1 = document.getElementById("C");
-        audio1.play();
+        var audio = document.getElementById("C");
+        audio.load();
+        audio.play();
     }
     if(event.key == "w"){ //C#
-        var audio2 = document.getElementById("Cs");
-        audio2.play();
+        var audio = document.getElementById("Cs");
+        audio.load();
+        audio.play();
     }
     if(event.key == "s"){ //D
-        var audio3 = document.getElementById("D");
-        audio3.play(); 
+        var audio = document.getElementById("D");
+        audio.load();
+        audio.play(); 
     }
     if(event.key == "e"){ //D#
-        var audio4 = document.getElementById("Ds");
-        audio4.play();
+        var audio = document.getElementById("Ds");
+        audio.load();
+        audio.play();
     }
     if(event.key == "d"){ //E
-        var audio5 = document.getElementById("E");
-        audio5.play();
+        var audio = document.getElementById("E");
+        audio.load();
+        audio.play();
     }
     if(event.key == "f"){ //F
-        var audio6 = document.getElementById("F");
-        audio6.play();
+        var audio = document.getElementById("F");
+        audio.load();
+        audio.play();
     }
     if(event.key == "t"){ //F#
-        var audio7 = document.getElementById("Fs");
-        audio7.play();
+        var audio = document.getElementById("Fs");
+        audio.load();
+        audio.play();
     }
     if(event.key == "g"){ //G
-        var audio8 = document.getElementById("G");
-        audio8.play();
+        var audio = document.getElementById("G");
+        audio.load();
+        audio.play();
     }
     if(event.key == "y"){ //G#
-        var audio9 = document.getElementById("Gs");
-        audio9.play();
+        var audio = document.getElementById("Gs");
+        audio.load();
+        audio.play();
     }
     if(event.key == "h"){ //A
-        var audio10 = document.getElementById("A");
-        audio10.play();
+        var audio = document.getElementById("A");
+        audio.load();
+        audio.play();
     }
     if(event.key == "u"){ //A#
-        var audio11 = document.getElementById("As");
-        audio11.play();
+        var audio = document.getElementById("As");
+        audio.load();
+        audio.play();
     }
     if(event.key == "j"){ //B
-        var audio12 = document.getElementById("B");
-        audio12.play();
+        var audio = document.getElementById("B");
+        audio.load();
+        audio.play();
     }
 });
 
@@ -60,62 +72,62 @@ function play(){
             console.log(array[i])
             if(array[i] == "C"){
                 var audio = document.getElementById("C");
-                audio.pause();
+                audio.load();
                 audio.play();
             }
             if(array[i] == "C#"){
                 var audio = document.getElementById("Cs");
-                audio.pause();
+                audio.load();
                 audio.play();
             }
             if(array[i] == "D"){
                 var audio = document.getElementById("D");
-                audio.pause();
+                audio.load();
                 audio.play(); 
             }
             if(array[i] == "D#"){
                 var audio = document.getElementById("Ds");
-                audio.pause();
+                audio.load();
                 audio.play();
             }
             if(array[i] == "E"){
                 var audio = document.getElementById("E");
-                audio.pause();
+                audio.load();
                 audio.play();
             }
             if(array[i] == "F"){
                 var audio = document.getElementById("F");
-                audio.pause();
+                audio.load();
                 audio.play();
             }
             if(array[i] == "F#"){
                 var audio = document.getElementById("Fs");
-                audio.pause();
+                audio.load();
                 audio.play();
             }
             if(array[i] == "G"){
                 var audio = document.getElementById("G");
-                audio.pause();
+                audio.load();
                 audio.play();
             }
             if(array[i] == "G#"){
                 var audio = document.getElementById("Gs");
-                audio.pause();
+                audio.load();
                 audio.play();
             }
             if(array[i] == "A"){
                 var audio = document.getElementById("A");
-                audio.pause();
+                audio.load();
                 audio.play();
             }
             if(array[i] == "A#"){
                 var audio = document.getElementById("As");
-                audio.pause();
+                audio.load();
                 audio.play();
             }
             if(array[i] == "B"){
                 var audio = document.getElementById("B");
-                audio.pause();
+                audio.load();
                 audio.play();
             }
             i++;
@@ -126,7 +138,7 @@ function play(){
                 document.getElementById("inputArray").disabled = false;
                 document.getElementById("play").disabled = false;
             }
-        }, 1600)
+        }, 600)
     }
     myLoop();
 }
